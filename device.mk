4db7b6ae71d7e7ac26aacce0c0fbce99f128406c
j0sh1x <aljoshua.hell@gmail.com>
2026-02-13 13:30:19 +0100

    h815: setup fstab

--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ LOCAL_PATH := device/lge/h815
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
 # Partitions
